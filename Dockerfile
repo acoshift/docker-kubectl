07a00c17b11ee34a1c01ea7dc1864015bd9ea958
acoshift <acoshift@gmail.com>
2018-09-01 23:07:22 +0700

    add entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM alpine
 LABEL maintainer="Thanatat Tamtan <acoshift@gmail.com>"
 
-RUN apk add --no-cache ca-certificates
+RUN apk add --no-cache bash ca-certificates
 
 ADD https://storage.googleapis.com/kubernetes-release/release/v1.10.7/bin/linux/amd64/kubectl /usr/bin/kubectl
-RUN chmod +x /usr/bin/kubectl && kubectl version --client
+RUN chmod +x /usr/bin/kubectl
+COPY kubectl.bash /usr/bin/kubectl.bash
 
-ENTRYPOINT ["/usr/bin/kubectl"]
+ENTRYPOINT ["/usr/bin/kubectl.bash"]
---
+++ kubectl.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ $(kubectl config current-context 2> /dev/null) == "" ]]; then
+    function usage() {
+        cat <<EOF
+No cluster is set. To set the cluster, set the environment variables
+  TOKEN=<service account token>
+  SERVER=<server master url>
+  NAMESPACE=<namespace>
+EOF
+        exit 1
+    }
+
+    [[ -z "$TOKEN" ]] && usage
+    [[ -z "$SERVER" ]] && usage
+    [[ -z "$NAMESPACE" ]] && usage
+
+    echo "Running: kubectl config set-credentials user --token=\"__TOKEN__\""
+	kubectl config set-credentials user --token="$TOKEN" || exit
+
+	echo "Running: kubectl config set-cluster cluster --server=\"__SERVER__\" --insecure-skip-tls-verify"
+	kubectl config set-cluster cluster-name --server="$SERVER" --insecure-skip-tls-verify || exit
+
+	echo "Running: kubectl config set-context context-name --cluster=cluster --user=user --namespace=\"__NAMESPACE__\""
+	kubectl config set-context context --cluster=cluster --user=user --namespace="$NAMESPACE" || exit
+
+	echo "Running: kubectl config use-context context"
+	kubectl config use-context context || exit
+fi
+
+kubectl "$@"
