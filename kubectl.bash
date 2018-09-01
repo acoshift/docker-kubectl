b72474a3117f99b08ff78ed9c2be782abe74bf95
acoshift <acoshift@gmail.com>
2018-09-02 00:12:32 +0700

    fix script

--- kubectl.bash
+++ kubectl.bash
@@ -18,10 +18,10 @@ EOF
     echo "Running: kubectl config set-credentials user --token=\"__TOKEN__\""
     kubectl config set-credentials user --token="$TOKEN" || exit
 
-    echo "Running: kubectl config set-cluster cluster --server=\"__SERVER__\" --insecure-skip-tls-verify"
-    kubectl config set-cluster cluster-name --server="$SERVER" --insecure-skip-tls-verify || exit
+    echo "Running: kubectl config set-cluster cluster --server=\"$SERVER\" --insecure-skip-tls-verify"
+    kubectl config set-cluster cluster --server="$SERVER" --insecure-skip-tls-verify || exit
 
-    echo "Running: kubectl config set-context context-name --cluster=cluster --user=user --namespace=\"__NAMESPACE__\""
+    echo "Running: kubectl config set-context context-name --cluster=cluster --user=user --namespace=\"$NAMESPACE\""
     kubectl config set-context context --cluster=cluster --user=user --namespace="$NAMESPACE" || exit
 
     echo "Running: kubectl config use-context context"
