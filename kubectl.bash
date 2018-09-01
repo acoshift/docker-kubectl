334547b08bcce67fe926362671b116cb48019d6b
acoshift <acoshift@gmail.com>
2018-09-01 23:08:34 +0700

    change indent to space

--- kubectl.bash
+++ kubectl.bash
@@ -16,16 +16,16 @@ EOF
     [[ -z "$NAMESPACE" ]] && usage
 
     echo "Running: kubectl config set-credentials user --token=\"__TOKEN__\""
-	kubectl config set-credentials user --token="$TOKEN" || exit
+    kubectl config set-credentials user --token="$TOKEN" || exit
 
-	echo "Running: kubectl config set-cluster cluster --server=\"__SERVER__\" --insecure-skip-tls-verify"
-	kubectl config set-cluster cluster-name --server="$SERVER" --insecure-skip-tls-verify || exit
+    echo "Running: kubectl config set-cluster cluster --server=\"__SERVER__\" --insecure-skip-tls-verify"
+    kubectl config set-cluster cluster-name --server="$SERVER" --insecure-skip-tls-verify || exit
 
-	echo "Running: kubectl config set-context context-name --cluster=cluster --user=user --namespace=\"__NAMESPACE__\""
-	kubectl config set-context context --cluster=cluster --user=user --namespace="$NAMESPACE" || exit
+    echo "Running: kubectl config set-context context-name --cluster=cluster --user=user --namespace=\"__NAMESPACE__\""
+    kubectl config set-context context --cluster=cluster --user=user --namespace="$NAMESPACE" || exit
 
-	echo "Running: kubectl config use-context context"
-	kubectl config use-context context || exit
+    echo "Running: kubectl config use-context context"
+    kubectl config use-context context || exit
 fi
 
 kubectl "$@"
